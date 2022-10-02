1718ac7694e2922691c72757c363a310e10e6af2
Ivan <Korneychik91@yandex.ru>
2022-10-02 21:55:52 +0300

    examples_6 done

---
+++ HomeWork/examples_6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введи целое число ");
+string usernum = Console.ReadLine () ??"";
+int num = int.Parse (usernum);
+
+if (num % 2 == 0)
+{
+    Console.WriteLine (num + " -> Четное");
+}
+else 
+{
+    Console.WriteLine (num + " -> Нечетное");
+}
