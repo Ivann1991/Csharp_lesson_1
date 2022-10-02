7a7e4df2cb2d180af74110d524e6730d98214094
Ivan <Korneychik91@yandex.ru>
2022-10-02 23:07:26 +0300

    examples_8 done

---
+++ HomeWork/examples_8/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введи целое число ");
+string usernum = Console.ReadLine() ?? "";
+int num = int.Parse(usernum);
+int a = 2;
+int b = -2;
+Console.Write("Четные числа от 1 до " + num + ": ");
+if (num > 0)
+{
+    while (a <= num)
+    {
+        Console.Write(a + "; ");
+        a = a + 2;
+    }
+}
+else
+{
+    while (b >= num)
+    {
+        Console.Write(b + "; ");
+        b = b - 2;
+    }
+}
+
