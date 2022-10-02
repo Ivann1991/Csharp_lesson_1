36b470813b1f84d621570bae78f9ff2de39a6baa
Ivan <Korneychik91@yandex.ru>
2022-10-02 19:01:02 +0300

    primer_1

---
+++ primer_1/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine ("Введи целое число");
+string user = Console.ReadLine () ?? "";
+int usernumber = int.Parse (user);
+Console.WriteLine (usernumber + " -> " + usernumber * usernumber);
