b0fbf0d5cce3464527afdc0fe7a271e7ca773bc7
Ivan <Korneychik91@yandex.ru>
2022-10-02 20:26:59 +0300

    primer_3

---
+++ primer_3/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.Write("Введи целое число от 1 до 7 ");
+string user = Console.ReadLine() ?? "";
+int usernum = int.Parse(user);
+if (usernum == 1)
+{
+    Console.WriteLine(usernum + " -> понедельник");
+}
+else 
+{
+    if (usernum == 2)
+    {
+      Console.WriteLine(usernum + " -> вторник");  
+    }
+    else
+    {
+        if (usernum == 3)
+    { 
+           Console.WriteLine(usernum + " -> среда"); 
+    }
+    else
+    {
+        if (usernum == 4)
+        {
+            Console.WriteLine(usernum + " -> четверг"); 
+        }
+        else
+        {
+            if (usernum == 5)
+            {
+                Console.WriteLine(usernum + " -> пятница");
+            }
+            else
+            {
+                if (usernum == 6)
+            {
+                Console.WriteLine(usernum + " -> суббота");
+            }
+            else
+            {
+                Console.WriteLine(usernum + " -> воскресенье");
+            }
+            }
+        } 
+    }}}
